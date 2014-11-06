5e75cf526cf3313523eec599f74e5f1af8b7b46a
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-06 11:14:52 +0100

    Cleanup tests

--- tests/test_internetdomain.py
+++ tests/test_internetdomain.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python
-#This file is part internetdomain module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part internetdomain module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
+import datetime
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
     test_depends
 from trytond.transaction import Transaction
 
-import datetime
 
 class DomainTestCase(unittest.TestCase):
-    '''
-    Test Internet Domain module.
-    '''
+    'Test Internet Domain module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('internetdomain')
@@ -34,22 +24,19 @@ class DomainTestCase(unittest.TestCase):
         self.user = POOL.get('res.user')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('internetdomain')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
-
     def test0010domain(self):
-        '''Create domain'''
-        with Transaction().start(DB_NAME, USER, context=CONTEXT) as transaction:
-            company, = self.company.search([('rec_name', '=', 'Dunder Mifflin')])
+        'Create domain'
+        with Transaction().start(DB_NAME, USER, context=CONTEXT):
+            company, = self.company.search([
+                    ('rec_name', '=', 'Dunder Mifflin'),
+                    ])
             self.user.write([self.user(USER)], {
                     'main_company': company.id,
                     'company': company.id,
@@ -78,7 +65,6 @@ class DomainTestCase(unittest.TestCase):
                 }])
             self.assert_(domain.id)
 
-            transaction.cursor.commit()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
@@ -89,6 +75,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         DomainTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
