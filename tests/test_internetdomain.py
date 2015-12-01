fc861be5eaa156e68a500de741a0e6e1c054f2ef
resteve <resteve@zikzakmedia.com>
2015-12-01 11:09:17 +0100

    Tests

--- tests/test_internetdomain.py
+++ tests/test_internetdomain.py
@@ -1,77 +1,18 @@
-#!/usr/bin/env python
-# This file is part internetdomain module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the internetdomain module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import datetime
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class DomainTestCase(unittest.TestCase):
-    'Test Internet Domain module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('internetdomain')
-        self.currency = POOL.get('currency.currency')
-        self.domain = POOL.get('internetdomain.domain')
-        self.renewal = POOL.get('internetdomain.renewal')
-        self.company = POOL.get('company.company')
-        self.party = POOL.get('party.party')
-        self.address = POOL.get('party.address')
-        self.user = POOL.get('res.user')
-
-    def test0005views(self):
-        'Test views'
-        test_view('internetdomain')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
-    def test0010domain(self):
-        'Create domain'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            company, = self.company.search([
-                    ('rec_name', '=', 'Dunder Mifflin'),
-                    ])
-            self.user.write([self.user(USER)], {
-                    'main_company': company.id,
-                    'company': company.id,
-                    })
-            CONTEXT.update(self.user.get_preferences(context_only=True))
-            party, = self.party.create([{
-                'name': 'Zikzakmedia',
-                'code': '001',
-                }])
-            address, = self.address.create([{
-                'name': 'Raimon Esteve',
-                'party': party.id,
-                }])
-            domain, = self.domain.create([{
-                'name': 'zikzakmedia.com',
-                'date_create': datetime.date(2011, 10, 1),
-                'party': party.id,
-                'party_address': address.id,
-                'company': company.id,
-                }])
-            self.renewal.create([{
-                'domain': domain.id,
-                'date_renewal': '2012-06-01',
-                'date_expire': '2013-06-01',
-                'registrator': company.id,
-                }])
-            self.assert_(domain.id)
+class InternetdomainTestCase(ModuleTestCase):
+    'Test Internetdomain module'
+    module = 'internetdomain'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.company.tests import test_company
-    for test in test_company.suite():
-        if test not in suite:
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        DomainTestCase))
+        InternetdomainTestCase))
     return suite
